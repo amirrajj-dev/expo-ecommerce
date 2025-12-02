341550ab8af1b50082a04b18582b9797f121ef62
amirrajj <amiramraja@gmail.com>
2025-12-02 14:20:06 +0330

    deployment config added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express, { type Request, type Response } from 'express';
+import path from 'path';
+import { ENV } from './configs/env';
 
 const app = express();
+const PORT = ENV.PORT || 3000;
+const __dirname = path.resolve();
 
-app.get('/health', (req: Request, res: Response) => {
+app.get('/api/health', (req: Request, res: Response) => {
   return res.status(200).json({
     messsage: 'OK',
     success: true,
@@ -10,6 +14,13 @@ app.get('/health', (req: Request, res: Response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log(`app is running on port ${3000}`);
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../admin/dist')));
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
 });
