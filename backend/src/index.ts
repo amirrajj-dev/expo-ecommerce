32e5195fff3b98f6ce8d6859bc18adae5900f5c9
amirrajj <amiramraja@gmail.com>
2025-12-06 21:36:26 +0330

    fix a dummy mistake

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -66,7 +66,7 @@ app.use(errorMiddleware);
 if (ENV.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../admin/dist')));
   app.get('/{*any}', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../admin', 'dist', 'index.html'));
   });
 }
 
