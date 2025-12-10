3154bc907efb6d04776a2474c1ba35df2c8e8dbc
amirrajj <amiramraja@gmail.com>
2025-12-10 16:34:13 +0330

    added categories array

---
+++ admin/src/data/data.tsx
@@ -0,0 +1,10 @@
+export const categories = [
+    {id : crypto.randomUUID() , content : "📱 Electronics" , value : "electronics"},
+    {id : crypto.randomUUID() , content : "🧸 Toys & Games" , value : "toys"},
+    {id : crypto.randomUUID() , content : "📚 Books & Media" , value : "books"},
+    {id : crypto.randomUUID() , content : "⚽ Sports & Outdoors" , value : "sports"},
+    {id : crypto.randomUUID() , content : "💄 Beauty & Personal Care" , value : "beauty"},
+    {id : crypto.randomUUID() , content : "👗 Fashion & Apparel" , value : "fashion"},
+    {id : crypto.randomUUID() , content : "🏠 Home & Kitchen" , value : "home"},
+    {id : crypto.randomUUID() , content : "🎮 Gaming & Entertainment" , value : "gaming"},
+]
