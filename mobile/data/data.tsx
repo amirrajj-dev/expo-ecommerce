da8f3877547369430300c22e50c034b0d5d38a47
amirrajj <amiramraja@gmail.com>
2025-12-14 20:57:10 +0330

    added categories array

---
+++ mobile/data/data.tsx
@@ -0,0 +1,10 @@
+export  const categories = [
+    { image: require("@/assets/images/electronic-devices.png"), name: "electronics" },
+    { image: require("@/assets/images/teddy-bear.png"), name: "toys" },
+    { image: require("@/assets/images/book.png"), name: "books" },
+    { image: require("@/assets/images/football.png"), name: "sports" },
+    { image: require("@/assets/images/personal.png"), name: "beauty" },
+    { image: require("@/assets/images/hoodie.png"), name: "fashion" },
+    { image: require("@/assets/images/house.png"), name: "home" },
+    { image: require("@/assets/images/console.png"), name: "gaming" },
+  ];
