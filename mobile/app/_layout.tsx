d8b8c0f3d35c1be8a03f41e58a95b68cf634c5d9
amirrajj <amiramraja@gmail.com>
2025-12-14 20:54:13 +0330

    added toast config

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,12 +3,15 @@ import "../global.css";
 import TanStackQueryProvider from "@/providers/tanstack-provider";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import Toast from "react-native-toast-message";
+import { toastConfig } from "@/components/toast/ToastConfig";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache} >
       <TanStackQueryProvider>
         <Stack screenOptions={{ headerShown: false }} />
+        <Toast config={toastConfig} />
       </TanStackQueryProvider>
     </ClerkProvider>
   );
