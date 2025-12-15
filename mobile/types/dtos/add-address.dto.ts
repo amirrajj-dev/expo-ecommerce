7c41d6688356f3e0abaefffc5610ff332a258848
amirrajj <amiramraja@gmail.com>
2025-12-15 21:06:31 +0330

    add necessay dtos for addresses

---
+++ mobile/types/dtos/add-address.dto.ts
@@ -0,0 +1,10 @@
+export interface AddAddressDto {
+    label : string;
+    fullName : string;
+    streetAddress : string;
+    city : string;
+    state : string;
+    zipCode : string;
+    phoneNumber : string;
+    isDefault : boolean
+}
