0897be6e2d85953049d75b322277c0bf13b5784e
amirrajj <amiramraja@gmail.com>
2025-12-15 15:19:59 +0330

    added other profile screens

---
+++ mobile/app/(profile)/orders.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SafeScreen from '@/components/SafeScreen'
+
+const OrdersScreen = () => {
+  return (
+    <SafeScreen>
+      <Text>OrdersScreen</Text>
+    </SafeScreen>
+  )
+}
+
+export default OrdersScreen
